0f515000727ace3e30a9e3081758f59a927f9869
Wolfgang Haupt <w.haupt@at-visions.com>
2014-03-12 07:42:20 +0100

    ADDED xvba build

--- buildPackage.sh
+++ buildPackage.sh
@@ -2,8 +2,8 @@
 
 BUILD=${1:-"all"}
 THREADS=32
-REL="./configure --prefix=/opt --enable-libusb --enable-vdpau --enable-vaapi --disable-joystick --disable-pulse --enable-airtunes --enable-airplay CXXFLAGS=-DTIXML_USE_STL --disable-debug"
-DBG="./configure --prefix=/opt --enable-libusb --enable-vdpau --enable-vaapi --disable-joystick --disable-pulse --enable-airtunes --enable-airplay CXXFLAGS=-DTIXML_USE_STL"
+REL="./configure --prefix=/opt --enable-libusb --disable-vdpau --disable-vaapi --disable-joystick --disable-pulse --enable-airtunes --enable-airplay CXXFLAGS=-DTIXML_USE_STL --disable-debug"
+DBG="./configure --prefix=/opt --enable-libusb --disable-vdpau --disable-vaapi --disable-joystick --disable-pulse --enable-airtunes --enable-airplay CXXFLAGS=-DTIXML_USE_STL"
 declare -x PATH="/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/lib/java/bin"
 GITREV=`git describe --tags`
 
@@ -27,10 +27,10 @@ function cfgbuild
 {
   if [ $1 == "release" ]; then
     CFG=$REL
-    OUTPUT="200gz-xbmc-$GITREV.lzm"
+    OUTPUT="200gz-xbmc-xvba-$GITREV.lzm"
   elif [ $1 == "debug" ]; then
     CFG=$DBG
-    OUTPUT="200gz-xbmc-$GITREV-debug.lzm"
+    OUTPUT="200gz-xbmc-xvba-$GITREV-debug.lzm"
   else
     echo "Unkown build - exit"
     exit 1
